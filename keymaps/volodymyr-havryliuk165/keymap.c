e63de965225cf5c8ada8c2532a354413649d7951
Volodymyr Havryliuk <vova2341591@gmail.com>
2023-05-14 18:14:52 +0300

    try 30 key layout

--- keymaps/volodymyr-havryliuk165/keymap.c
+++ keymaps/volodymyr-havryliuk165/keymap.c
@@ -89,9 +89,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_SOUL] = LAYOUT_split_3x5_3(
-      KC_Q, KC_W, KC_L, KC_D, KC_P, KC_K, KC_M, KC_U, KC_Y, KC_UNDS,
+      KC_Q, KC_W, KC_L, KC_D, KC_P, KC_K, KC_M, KC_U, KC_Y, KC_J,
       KC_A, KC_S, KC_R, KC_T, KC_G, KC_F, KC_N, KC_E, KC_I, KC_O,
-      KC_J, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_H, KC_COMM, KC_DOT, KC_QUES,
+      XXXXXXX, KC_Z, KC_X, KC_C, XXXXXXX, XXXXXXX, KC_H, KC_V, KC_B, XXXXXXX,
       XXXXXXX, OSL(_NAVIGATION), KC_SPC, KC_BSPC, OSL(_SYMBOL), XXXXXXX
     ),
 
@@ -110,30 +110,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_NAVIGATION] = LAYOUT_split_3x5_3(
-      KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
-      OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_LSFT), _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_ESC, 
-      KC_PPLS, KC_PMNS, KC_PEQL, KC_PSLS, KC_PAST, KC_TAB, REPEAT, ALTREP, KC_PDOT, _______,
-      XXXXXXX, _______, OSL(_LANGUAGE), KC_ENT, MO(_SYSTEM), _______ 
+      KC_ENT, ALTREP, KC_UP, REPEAT, XXXXXXX, XXXXXXX, KC_7, KC_8, KC_9, OSM(MOD_LCTL),
+      KC_ESC, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TAB, OSM(MOD_LALT), KC_4, KC_5, KC_6, OSM(MOD_LSFT), 
+      XXXXXXX, KC_PLUS, KC_ASTR, KC_PDOT, KC_EQL, OSM(MOD_LGUI), KC_1, KC_2, KC_3, XXXXXXX,
+      XXXXXXX, _______, OSL(_LANGUAGE), KC_0, MO(_SYSTEM), XXXXXXX
     ),
 
     [_SYMBOL] = LAYOUT_split_3x5_3(
-      KC_TILD, KC_AT, KC_CIRC, KC_DLR,    KC_PERC,       KC_GRV, KC_LPRN, KC_RPRN, KC_QUOT, KC_QUES, 
-      KC_PLUS, KC_MINUS, KC_EQL, KC_COLN, KC_ASTR,       KC_BSLS, KC_LCBR, KC_RCBR, KC_DQUO, KC_SCLN,
-      KC_LT, KC_EXLM, KC_PIPE, KC_GT,     KC_AMPR,       KC_DOT, KC_LBRC, KC_RBRC, KC_HASH, KC_SLSH,
-      XXXXXXX, MO(_SYSTEM), _______, _______, _______, XXXXXXX 
+      KC_PERC, KC_TILD, KC_CIRC, KC_DLR, XXXXXXX,      XXXXXXX, KC_LPRN, KC_HASH, KC_RPRN, KC_GRV, 
+      KC_LT, KC_MINUS, KC_EQL, KC_COLN, KC_ASTR,       KC_BSLS, KC_LBRC, KC_QUOT, KC_RBRC,  KC_SCLN,
+      XXXXXXX, KC_EXLM, KC_PIPE, KC_GT, KC_AMPR,       KC_AT, KC_COMM, KC_DOT, KC_SLSH, XXXXXXX,
+      XXXXXXX, MO(_SYSTEM), OSM(MOD_LSFT), XXXXXXX, _______, XXXXXXX 
     ),
 
     [_SYSTEM] = LAYOUT_split_3x5_3(
-      KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-      _______, _______, _______, _______, KC_PSCR, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______,
-      _______, _______, KC_DEL, KC_INS,  _______, _______, _______, _______, _______, _______,
-      _______, _______, KC_F11, KC_F12, _______, _______ 
+      _______, _______, KC_PGUP, _______, XXXXXXX,     XXXXXXX, KC_F7, KC_F8, KC_F9, KC_F10,
+      _______, KC_HOME, KC_PGDN, KC_END, KC_PSCR,      KC_F12, KC_F4, KC_F5, KC_F6, KC_F11,
+      XXXXXXX, _______, KC_DEL, KC_INS,  _______,      _______, KC_F1, KC_F2, KC_F3, XXXXXXX,
+      XXXXXXX, _______, _______, _______, _______, XXXXXXX 
     ), 
 
     [_LANGUAGE] = LAYOUT_split_3x5_3(
-      _______, DF(_QWERTY), DF(_SOUL), DF(_UN), _______, _______, _______, _______, _______, _______,
+      _______, DF(_QWERTY), DF(_SOUL), DF(_UN), XXXXXXX, XXXXXXX, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX,
       XXXXXXX, _______, _______, _______, _______, XXXXXXX 
     )
 
