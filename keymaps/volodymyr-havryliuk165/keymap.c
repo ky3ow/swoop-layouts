ec5af3db273b4992fcf27b7bef4c1777a0d65a93
Volodymyr Havryliuk <vova2341591@gmail.com>
2023-06-13 19:42:32 +0300

    v11

--- keymaps/volodymyr-havryliuk165/keymap.c
+++ keymaps/volodymyr-havryliuk165/keymap.c
@@ -7,15 +7,13 @@
 enum layers {
     // Base layers
     _QWERTY = 0,
-    _SOUL,
     _UN,
-    _UN_EXTRA,
     // Right hand layers
     _NAV,
     _FUNC,
     _MISC,
     // Left hand layers
-    _EXPR
+    _SYM
 };
 
 enum custom_keycodes {
@@ -23,15 +21,17 @@ enum custom_keycodes {
     REPEAT = SAFE_RANGE,
     ALTREP,
     // One shot layers because OSL is broken rn
+    OS_SYM,
     OS_NAV,
     OS_FUNC,
-    OS_MISC,
-    OS_EXPR
+    OS_MISC
 };
 
-#define DF_SOUL DF(_SOUL)
 #define DF_QWER DF(_QWERTY)
 #define DF_UKR DF(_UN)
+
+#define CTL_SPC MT(MOD_LCTL, KC_SPC)
+#define SFT_BSPC MT(MOD_LSFT, KC_BSPC)
 
 #define OS_SFT OSM(MOD_LSFT)
 #define OS_CTL OSM(MOD_LCTL)
@@ -50,23 +50,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
        KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,                          KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
-       KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,                          KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN , 
+       KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,                          KC_H    ,KC_J    ,KC_K    ,KC_L    ,REPEAT  , 
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
-       KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,                          KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_QUES , 
+       KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,                          KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SCLN , 
     //└─────────────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┘
-                         XXXXXXX ,OS_NAV  ,KC_SPC  ,                          REPEAT  ,OS_EXPR ,XXXXXXX
-    //                  └────────┴────────┴────────┘                         └────────┴────────┴────────┘
-    ),
-
-    [_SOUL] = LAYOUT_split_3x5_3(
-    //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
-       KC_Q    ,KC_W    ,KC_L    ,KC_D    ,KC_P    ,                          KC_K    ,KC_M    ,KC_U    ,KC_Y    ,KC_QUOT ,
-    //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
-       KC_A    ,KC_S    ,KC_R    ,KC_T    ,KC_G    ,                          KC_F    ,KC_N    ,KC_E    ,KC_I    ,KC_O    , 
-    //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
-       KC_J    ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,                          KC_B    ,KC_H    ,KC_COMM ,KC_DOT  ,KC_SCLN , 
-    //└─────────────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┘
-                         XXXXXXX ,OS_NAV  ,KC_SPC  ,                          REPEAT  ,OS_EXPR ,XXXXXXX
+                         XXXXXXX ,OS_NAV  ,CTL_SPC ,                          SFT_BSPC,OS_SYM  ,XXXXXXX 
     //                  └────────┴────────┴────────┘                         └────────┴────────┴────────┘
     ),
 
@@ -78,19 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
        KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,                          KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_QUES , 
     //└─────────────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┘
-                         XXXXXXX ,OS_NAV  ,KC_SPC  ,                          REPEAT  ,OS_EXPR ,XXXXXXX
-    //                  └────────┴────────┴────────┘                         └────────┴────────┴────────┘
-    ),
-
-    [_UN_EXTRA] = LAYOUT_split_3x5_3(
-    //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
-       _______ ,_______ ,_______ ,_______ ,_______ ,                          _______ ,_______ ,_______ ,_______ ,_______ ,
-    //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
-       _______ ,_______ ,_______ ,_______ ,_______ ,                          _______ ,_______ ,_______ ,_______ ,_______ , 
-    //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
-       _______ ,_______ ,_______ ,_______ ,_______ ,                          _______ ,_______ ,_______ ,_______ ,_______ , 
-    //└─────────────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┘
-                         XXXXXXX ,_______ ,_______ ,                          _______ ,_______ ,XXXXXXX
+                         XXXXXXX ,OS_NAV  ,CTL_SPC ,                          SFT_BSPC,OS_SYM  ,XXXXXXX 
     //                  └────────┴────────┴────────┘                         └────────┴────────┴────────┘
     ),
 
@@ -102,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
        _______ ,_______ ,OS_MISC ,OS_FUNC ,_______ ,                          KC_TAB  ,_______ ,_______ ,_______ ,_______ , 
     //└─────────────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┘
-                         XXXXXXX ,_______ ,KC_BSPC ,                          KC_ENT  ,_______ ,XXXXXXX
+                         XXXXXXX ,_______ ,_______ ,                          KC_ENT  ,_______ ,XXXXXXX
     //                  └────────┴────────┴────────┘                         └────────┴────────┴────────┘
     ),
 
@@ -120,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_MISC] = LAYOUT_split_3x5_3(
     //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
-       _______ ,_______ ,_______ ,_______ ,_______ ,                          _______ ,DF_QWER ,DF_SOUL ,DF_UKR  ,_______ ,
+       _______ ,_______ ,_______ ,_______ ,_______ ,                          _______ ,DF_QWER ,_______ ,DF_UKR  ,_______ ,
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
        _______ ,_______ ,_______ ,_______ ,_______ ,                          _______ ,_______ ,_______ ,_______ ,_______ , 
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
@@ -130,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //                  └────────┴────────┴────────┘                         └────────┴────────┴────────┘
     ),
 
-    [_EXPR] = LAYOUT_split_3x5_3(
+    [_SYM] = LAYOUT_split_3x5_3(
     //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
        KC_PERC ,KC_TILD ,KC_CIRC ,KC_DLR  ,KC_AT   ,                          KC_GRV  ,KC_LPRN ,KC_LBRC ,KC_RBRC ,KC_RPRN ,
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
@@ -138,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
        KC_LT   ,KC_EXLM ,KC_PIPE ,KC_GT   ,KC_AMPR ,                          KC_UNDS ,KC_DQUO ,KC_QUES ,_______ ,KC_SLSH , 
     //└─────────────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┘
-                         XXXXXXX ,KC_BSLS ,KC_BSPC ,                          _______ ,_______ ,XXXXXXX
+                         XXXXXXX ,KC_BSLS ,_______ ,                          _______ ,_______ ,XXXXXXX
     //                  └────────┴────────┴────────┘                         └────────┴────────┴────────┘
     )
 };
@@ -151,7 +127,7 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
     uint8_t layer         = get_highest_layer(state);
     bool    should_toggle = false;
     switch (layer) {
-        case _QWERTY ... _SOUL:
+        case _QWERTY:
             should_toggle = prev_layer >= _UN;
             break;
         case _UN:
@@ -166,10 +142,10 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     static layer_state_t prev_state    = 0;
-    bool                 base_to_layer = layer_state_cmp(prev_state, 0) && !layer_state_cmp(state, 0);
-    bool                 layer_to_base = !layer_state_cmp(prev_state, 0) && layer_state_cmp(state, 0);
+    bool                 base_to_sym = layer_state_cmp(prev_state, 0) && layer_state_cmp(state, _SYM);
+    bool                 sym_to_base = layer_state_cmp(state, 0) && layer_state_cmp(prev_state, _SYM);
     bool                 is_un         = get_highest_layer(default_layer_state) == _UN;
-    if (is_un && (base_to_layer || layer_to_base)) {
+    if (is_un && (base_to_sym || sym_to_base)) {
         CHANGE_LANG;
     }
 
@@ -191,18 +167,18 @@ bool is_oneshot_mod_key(uint16_t keycode) {
     return false;
 }
 
-oneshot_state os_nav_state  = os_up_unqueued;
+oneshot_state os_sym_state = os_up_unqueued;
+oneshot_state os_nav_state = os_up_unqueued;
 oneshot_state os_func_state = os_up_unqueued;
 oneshot_state os_misc_state = os_up_unqueued;
-oneshot_state os_expr_state = os_up_unqueued;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_repeat_key_with_alt(keycode, record, REPEAT, ALTREP)) { return false; }
     bool handled = true;
+    handled      = update_oneshot_layer(&os_sym_state, _SYM, OS_SYM, keycode, record) & handled;
     handled      = update_oneshot_layer(&os_nav_state, _NAV, OS_NAV, keycode, record) & handled;
     handled      = update_oneshot_layer(&os_func_state, _FUNC, OS_FUNC, keycode, record) & handled;
     handled      = update_oneshot_layer(&os_misc_state, _MISC, OS_MISC, keycode, record) & handled;
-    handled      = update_oneshot_layer(&os_expr_state, _EXPR, OS_EXPR, keycode, record) & handled;
 
     if (!handled) return false;
 
